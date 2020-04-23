10cb64e5ac31b3a168163e300498aa3067c88d4f
DiegoFHG <34254918+DiegoFHG@users.noreply.github.com>
2020-04-24 00:16:33 +0200

    Added routing.

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,78 @@
 <script>
-	export let name;
+  import { Router, Link, Route } from 'svelte-routing';
+  import TabBar from './components/TabBar.svelte';
+
+  export let url = '';
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&display=swap');
+
   :global(body) {
-    margin: 0;
-    padding: 0;
     background-color: #3B4252;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  main {
+    padding-top: 10px;
+    margin-top: 60px;
+    color: white;
+    padding-left: 10%;
+    padding-right: 10%;
   }
 
   .app-header {
+    z-index: 10;
+    overflow: hidden;
+    top: 0;
     background-color: #2E3440;
-    height: 40px;
+    display: flex;
+    justify-content: space-between;
+    height: 50px;
     box-shadow: rgba(36, 41, 51, 0.15) 0px 5px 10px 0px;;
+    position: fixed;
+    width: 100%;
+  }
+
+  .app-title-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: white;
+    font-weight: 700;
+    font-size: 20px;
+    margin-left: 15px;
+  }
+
+  .app-tab-container {
+    display: flex;
+    margin-right: 15px;
   }
 </style>
 
-<header class="app-header">
-</header>
+<Router url={url}>
+  <header class="app-header">
+    <div class="app-title-container">
+      <span>File Indexer</span>
+    </div>
+    <div class="app-tab-container">
+      <TabBar route="/">
+        Inicio
+      </TabBar>
+      <TabBar route="/index">
+        Indexear
+      </TabBar>
+    </div>
+  </header>
+  <div>
+    <main>
+      <Route path="/">
+        <h1>Inicio</h1>
+      </Route>
+      <Route path="/index">
+        <h1>Indexear</h1>
+      </Route>
+    </main>
+  </div>
+</Router>
 
-<main>
-</main>
