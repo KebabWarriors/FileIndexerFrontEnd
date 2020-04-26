6e4c5503a1d353190f1f26bd44d8689318b6f84b
DiegoFHG <34254918+DiegoFHG@users.noreply.github.com>
2020-04-26 14:38:58 +0200

    Finished Search page design.

--- src/components/Options.svelte
+++ src/components/Options.svelte
@@ -1,5 +1,6 @@
 <script>
   import Checkbox from './Checkbox.svelte';
+  import PathsBar from './PathsBar.svelte';
 </script>
 
 <style lang="scss">
@@ -8,6 +9,9 @@
 
 <div>
   <h3>Directorios</h3>
+  <PathsBar />
+  <br />
+  <small style="font-weight: 100;"><i><b>Separados por comas</b></i></small>
   <br />
   <h3>Tipos de archivos</h3>
   <Checkbox label=".docx" />
---
+++ src/components/PathsBar.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Icon from 'mdi-svelte';
+  import { mdiFileMultiple } from '@mdi/js';
+  import InputBar from './InputBar.svelte';
+</script>
+
+<InputBar>
+  <div slot="icon">
+    <Icon path={mdiFileMultiple} color="#5E81AC" />
+  </div>
+  <input slot="input" placeholder="Ex. ~/Documents" />
+</InputBar>
